7dbc2529e24fb3d6eb76282fcdf9073a303ed32b
DELL <DELL@DESKTOP-40V68D5>
2020-02-22 18:43:54 +0530

    First

---
+++ src/main/java/com/dilu/test/TestGithub.java
@@ -0,0 +1,32 @@
+package com.dilu.test;
+
+public class TestGithub {
+
+	private Integer empNo;
+	private String empName;
+	private Double empSal;
+
+	public Integer getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(Integer empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Double getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+}
